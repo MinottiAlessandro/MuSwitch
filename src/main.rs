5bd4c0f057ad6bfd6576287a64959156e6b01126
Alessandro Minotti <alessandro.minotti@lobra.com>
2024-05-22 00:17:30 +0200

    Initial implementation of spotify playlist songs retrieving

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize};
 use chrono::{DateTime, Utc, Duration};
-use std::sync::Mutex;
+use std::{sync::Mutex};
 use dotenv::dotenv;
-use std::env;
+use std::{env, io};
 
 
 #[derive(Deserialize, Debug)]
@@ -81,12 +81,43 @@ async fn fetch_token(client: &reqwest::Client, cache: &Mutex<AuthResponseCache>)
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // --- SPOTIFY AUTH ---
+    // --- SPOTIFY ---
     let client = reqwest::Client::new();
     let token_cache = Mutex::new(AuthResponseCache::new());
     
     let access_token = fetch_token(&client, &token_cache).await?;
-    println!("{access_token}");
+
+    loop {
+        let mut playlist_id = String::new();
+
+        println!("playlist ID (insert q to exit): ");
+        
+        io::stdin()
+            .read_line(&mut playlist_id)
+            .expect("Error reading input");
+        
+        playlist_id = playlist_id.trim().to_string();
+
+        if playlist_id.to_lowercase() == "q".to_string() {
+            println!("Exiting...");
+            break;
+        }
+
+        println!("Getting the playlist: {playlist_id}");
+        
+        let request_url = format!("https://api.spotify.com/v1/playlists/{}/tracks", playlist_id);
+        let response_body = client.get(request_url)
+            .bearer_auth(&access_token)
+            .send()
+            .await?
+            .text()
+            .await?;
+
+        let parsed_json: serde_json::Value = serde_json::from_str(&response_body)?;
+        for i in 0..(parsed_json["total"].as_i64().unwrap() as usize) {
+            println!("{} - {}", parsed_json["items"][i]["track"]["artists"][0]["name"], parsed_json["items"][i]["track"]["name"]);
+        }
+    }
 
     return Ok(());
 }
