09596723074580ac47a9453da26d3d2136f84322
Alessandro Minotti <alessandro.minotti@lobra.com>
2024-05-22 11:28:25 +0200

    added cmd line arguments support

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,52 @@
 use std::sync::Mutex;
-use std::io;
+use clap::{App, Arg};
 mod models;
 mod utils;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut app = App::new("My Program")
+    .version("0.1.0")
+    .author("Alessandro Minotti")
+    .about("A tool to copy playlist")
+    .arg(
+        Arg::with_name("playlistID")
+            .short('p')
+            .long("playlist")
+            .required(true)
+            .takes_value(true)
+            .help("Playlist to retrieve"),
+    );
+
+    if app.clone().get_matches().is_present("help") {
+        app.print_help().unwrap();
+        return Ok(());
+    }
+
+    let playlist_id = app.get_matches().value_of("playlistID").unwrap().to_string();
+
     // --- SPOTIFY ---
     let client = reqwest::Client::new();
     let token_cache = Mutex::new(models::spotify::AuthResponseCache::new());
-    
     let access_token = utils::web_interfaces::fetch_token(&client, &token_cache).await?;
 
-    loop {
-        let mut playlist_id = String::new();
+    println!("Getting the playlist: {playlist_id}");
+    
+    let request_url = format!("https://api.spotify.com/v1/playlists/{}/tracks", playlist_id);
+    let response_body: models::spotify::Playlist = client.get(request_url)
+        .bearer_auth(&access_token)
+        .send()
+        .await?
+        .json()
+        .await?;
 
-        println!("playlist ID (insert q to exit): ");
-        
-        io::stdin()
-            .read_line(&mut playlist_id)
-            .expect("Error reading input");
-        
-        playlist_id = playlist_id.trim().to_string();
+    for elem in response_body.items {
+        let artists: Vec<String> = elem.track.artists.iter()
+            .map(|artist| artist.name.to_string())
+            .collect();
 
-        if playlist_id.to_lowercase() == "q".to_string() {
-            println!("Exiting...");
-            break;
-        }
-
-        println!("Getting the playlist: {playlist_id}");
-        
-        let request_url = format!("https://api.spotify.com/v1/playlists/{}/tracks", playlist_id);
-        let response_body: models::spotify::Playlist = client.get(request_url)
-            .bearer_auth(&access_token)
-            .send()
-            .await?
-            .json()
-            .await?;
-
-        for elem in response_body.items {
-            let mut artists: Vec<String> = Vec::new();
-            for artist in elem.track.artists {
-                artists.push(artist.name.to_string());
-            }
-            println!("{} -> {}", artists.join(", "), elem.track.name);
-        }
+        println!("{} -> {}", artists.join(", "), elem.track.name);
     }
 
     return Ok(());
